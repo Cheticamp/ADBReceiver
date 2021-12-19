bd0d146b038c53a08d8c39bdc7e7c40d84e976da
Cheticamp <shopn08@gmail.com>
2021-12-19 16:09:32 -0500

    Implement character-by-character insertion for API 29-

--- app/src/androidTest/java/com/example/adbreceiver/AdbInterface.kt
+++ app/src/androidTest/java/com/example/adbreceiver/AdbInterface.kt
@@ -1,24 +1,88 @@
 package com.example.adbreceiver
 
 /*
- * Test that runs with a broadcast receiver that accepts commands and Unicode encoded text.
+ * A stand-alone automation test that runs with a broadcast receiver that accepts Unicode text
+ * from adb for display. This project runs as an instrumented test on Android to gain access to
+ * the functionality of UI Automator (https://developer.android.com/training/testing/ui-automator)
  *
- * To start the test:
- * adb shell nohup am instrument -w com.example.adbreceiver.test/androidx.test.runner.AndroidJUnitRunner
+ * Reason for this project: A common way to send character input into Android through adb for
+ * testing is to use the "adb shell input [text]" command. This works well if the text is
+ * ASCII but fails if the text is non-ASCII. This project is an attempt to overcome this
+ * restriction by permitting non-ASCII Unicode characters to be delivered to the Android
+ * system through the adb bridge.
+ *
+ * To start the test from the command line (recommended):
+ *
+ *      adb shell "nohup am instrument -w com.example.adbreceiver.test/androidx.test.runner.AndroidJUnitRunner &"
+ *
+ * If you are presented with a prompt, something like "com.example.adbreceiver.AdbInterface:",
+ * you can abort out of it with ctrl-c (Windows) to return to the local terminal prompt. The test
+ * should continue to run in the background even if adb and Android Studio are shut down and should
+ * continue to run until it receives a stop command (adb shell am broadcast - a ADB_STOP).
  *
  * On Windows, the code page may need to be changed to UTF-8 by using the following command:
  *      chcp 65001
  *
- * Thanks to the GitHub project ADBKeyboard (https://github.com/senzhk/ADBKeyBoard) for the idea
- * of a background receiver.
+ * Commands are delivered through adb and the Android shell as follows:
+ *
+ *      adb shell am broadcast -a [command] [--es msg 'text']
+ *
+ * where [command] [-ed msg 'text'] is one of the following.
+ *
+ * ADB_CLEAR_TEXT - clears the text from the currently focussed field.
+ *
+ * ADB_INPUT_TEXT --es msg '[text]' where [text] is any text that can be produced by the
+ *      currently active physical keyboard. Go to "Settings" to change the physical keyboard.
+ *
+ * ADB_INPUT_B64 -es msg '[base64Text]' where '[base64Text]' is the base-64 representation
+ *      of the text to be entered.
+ *
+ * For the input commands, an attempt is made to inject the key events for the specified
+ * text using the UI Automator for APIs 29 and below. For these APIs, the text will be presented
+ * by Android as if the text came from the physical keyboard that is currently active. The key
+ * events will be interpreted against the key character maps of the active physical keyboard for
+ * display. The interface will not display any characters that cannot be produced by the physical
+ * keyboard.
+ *
+ * For API 30 and up (currently just 31), the generated key events always produce an
+ * ASCII result, so the text is sent directly to the view system through UI Automator for display.
+ * The result is that, for an EditText, the entire contents of the view will be replaced. The
+ * inability to create KeyEvents that register correctly with the display seems to be a change
+ * in the behavior of the Android framework. As a result, text sent for display may have no effect
+ * if there is not an EditText (or similar) field that has focus.
+ *
+ * For instance, to send the Hebrew characters `שלום', do the following:
+ *
+ * 1. Make sure the current physical keyboard has "Hebrew" defined as the language.
+ * 2. Start AdbInterface
+ *          adb shell "nohup am instrument -w com.example.adbreceiver.test/androidx.test.runner.AndroidJUnitRunner &"
+ * 3. Start any app and set the focus on a field that can accept the characters.
+ * 4. Execute the following command from a terminal session:
+ *          adb shell am broadcast -a ADB_INPUT_TEXT --es msg 'שלום'
+ * 5. The characters will appear in the field either one-by-one (API 29-) or immediately as
+ *    replacement text (for API 30+)
+ *
+ * Important! Make sure that the target that will receive the generated key events is not
+ * part of this app since AdbInterface will fail.
+ *
+ * Credit goes to the GitHub project ADBKeyboard (https://github.com/senzhk/ADBKeyBoard) for the
+ * idea of a background receiver. In the author's opinion, ADBKeyboard is a better way to
+ * implement the key event functionality, although it has the downside of requiring a keyboard
+ * change.
+ *
+ * Also, see
+ * https://stackoverflow.com/questions/70160582/can-accessibilityservice-dispatch-key-events-including-even-unicode-characters
  *
  */
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.util.Base64
+import android.util.Log
 import android.view.KeyCharacterMap
+import android.view.KeyEvent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
@@ -34,13 +98,25 @@ class AdbInterface {
     private var mDevice: UiDevice? = null
     private var mStop = false
 
+    // Accept text input. The characters received should be characters that the current active
+    // physical keyboard can produce.
     private val ACTION_MESSAGE = "ADB_INPUT_TEXT"
+
+    // Accept text input that is encoded in base-64.
     private val ACTION_MESSAGE_B64 = "ADB_INPUT_B64"
+
+    // Clear the text field that has the current focus.
     private val ACTION_CLEAR_TEXT = "ADB_CLEAR_TEXT"
+
+    // Stop this test and exit.
     private val ACTION_STOP = "ADB_STOP"
 
     private var mReceiver: BroadcastReceiver? = null
     private val mInstrumentation = InstrumentationRegistry.getInstrumentation()
+
+    private val mVirtualKCMap by lazy {
+        KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD)
+    }
 
     @Test
     fun adbListener() {
@@ -60,35 +136,8 @@ class AdbInterface {
         }
     }
 
-    // Enter text into the currently focussed field. If not field has focus, ignore the message.
-    private fun inputMsg(s: String?) {
-        s ?: return
-
-        val keyCharacterMap = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD)
-        val events = keyCharacterMap.getEvents(s.toCharArray())
-        if (events == null) {
-            // Unicode characters? Can't inject these until we determine how to do it.
-            // The following will replace the entire focussed field.
-            setFocussedText(s)
-        } else {
-            // Probably ASCII, so character-by-character insertion works.
-            for (e in events) {
-                mInstrumentation.uiAutomation.injectInputEvent(e, true)
-            }
-        }
-    }
-
-    private fun clearText() {
-        setFocussedText(null)
-    }
-
-    // findObject has a lot of capabilities and the broadcasts can be greatly expanded to do more.
-    // Here, we will just concentrate on the focussed field.
-    private fun setFocussedText(s: String?) {
-        mDevice?.findObject(By.focused(true))?.text = s
-    }
-
     private fun runUntilAskedToStop() {
+        Log.d(TAG, "AdbInterface started")
         try {
             // Keep us running to receive commands.
             // Really not a good way to go, but it works for the proof of concept.
@@ -98,6 +147,47 @@ class AdbInterface {
         } catch (e: InterruptedException) {
             e.printStackTrace()
         }
+        Log.d(TAG, "Stopped")
+    }
+
+    private fun inputMsg(s: String?) {
+
+        if (s.isNullOrEmpty()) return
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // The logic to map the characters fails to work for API 30+, so we can just replace
+            // the contents of the currently focussed text field.
+            setFocussedText(s)
+        } else {
+            //  Create a KeyEvent that uses the current active keyboard's KeyCharacterMap. If that
+            //  doesn't work, try with the virtual character map (ASCII only); otherwise, just
+            //  replace the text in the focussed field.
+
+            // Build a dummy KeyEvent to extract the KeyCharacterMap.
+            val keyEvent = KeyEvent(0L, 0L, KeyEvent.ACTION_DOWN, 29, 0, 0, 0, 0)
+            val charArray = s.toCharArray()
+            val events =
+                keyEvent.keyCharacterMap?.getEvents(charArray) ?: mVirtualKCMap.getEvents(charArray)
+            if (events != null) {
+                injectEvents(events)
+            } else {
+                setFocussedText(s)
+            }
+        }
+    }
+
+    private fun injectEvents(events: Array<out KeyEvent?>) {
+        for (e in events) {
+            mInstrumentation.uiAutomation.injectInputEvent(e, true)
+        }
+    }
+
+    private fun clearText() {
+        setFocussedText(null)
+    }
+
+    private fun setFocussedText(s: String?) {
+        mDevice?.findObject(By.focused(true))?.text = s
     }
 
     internal inner class AdbReceiver : BroadcastReceiver() {
@@ -135,5 +225,9 @@ class AdbInterface {
                 }
             }
         }
+    }
+
+    companion object {
+        const val TAG = "AdbInterface"
     }
 }
--- app/src/main/java/com/example/adbreceiver/MainActivity.kt
+++ app/src/main/java/com/example/adbreceiver/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.adbreceiver
 
-import androidx.appcompat.app.AppCompatActivity
+// See comments in AdbInteface in androidTest of this project.
+
+// Use another app to receive test data. Using this same app for the AdbInterface and
+// command testing will not work.
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.adbreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
